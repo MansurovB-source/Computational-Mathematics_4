0a82c00cc099a9a114267a9f0357212346785908
Behruz Mansurov <bekha.m2000.bm@gmail.com>
2020-04-23 20:21:52 +0300

    Added all functions

--- src/Function.java
+++ src/Function.java
@@ -1,8 +1,23 @@
+import static java.lang.Math.cos;
+import static java.lang.Math.pow;
+
 public class Function {
     private int func_num;
 
     void setFunc_num(int func_num) {
         this.func_num = func_num;
+    }
+
+    double f(double x, double y) {
+        if(func_num == 1) {
+            return pow((y + x), 2);
+        } else if (func_num == 2) {
+            return  x + cos(y / (pow(10, 0.5)));
+        } else if (func_num == 3) {
+            return x + y;
+        } else {
+            return 1;
+        }
     }
 
     void showFunction() {
